5619c3f92deb5a601dee458988a73b21fbe93b46
Ильхам Муратшин <PETA4OK96@mail.ru>
2021-10-21 21:42:22 +0500

    Add project files.

---
+++ laba1/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Ученик_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0)
+            {
+                MessageBox.Show("Введите фамилию");
+                return;
+            }
+            else if (textBox2.Text.Length == 0)
+            {
+                MessageBox.Show("Введите имя");
+                return;
+            }
+            else if (comboBox1.Text.Length == 0)
+            {
+                MessageBox.Show("Выберите класс");
+                return;
+            }
+            else if (!radioButton2.Checked & !radioButton1.Checked)
+            {
+                MessageBox.Show("Выберите уровень сложности");
+                return;
+            }
+            Form2 tf = new Form2(
+                textBox2.Text,
+                textBox1.Text,
+                comboBox1.Text,
+                numericUpDown1.Value,
+                radioButton1.Checked);
+            // radioButton1.Checked
+            tf.ShowDialog();
+
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ laba1/Form2.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace laba1
+{
+    public partial class Form2 : Form
+    {
+        public Form2(string name, string secondName, string number_class, decimal count, bool simple)
+        {
+            InitializeComponent();
+            //груп бокс
+            var groupBox = new GroupBox();
+            groupBox.Dock = DockStyle.Fill;
+            groupBox.Text = $"{secondName} {name} {number_class}";
+
+            //столбцы и строчки 
+            var column = new TableLayoutPanel();
+            column.Dock = DockStyle.Fill;
+            column.ColumnCount = 2;
+            column.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33));
+            column.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 66));
+            column.RowCount = ((int)count) + 1;
+            column.AutoScroll = true;
+            
+            
+            groupBox.Controls.Add(column);
+
+            for (int r = 0; r < column.RowCount - 1; ++r)
+            {
+                var radio = new TableLayoutPanel();
+                radio.Dock = DockStyle.Top;
+                radio.ColumnCount = 2;
+                radio.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
+                radio.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));
+                //radio.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+                var task = new Label();
+                task.Dock = DockStyle.Top;
+                if (simple)
+                {
+                    task.Text = $"Изи #{r + 1}";
+                }
+                else
+                {
+                    task.Text = $"Хард #{r + 1}";
+                }
+
+
+                column.Controls.Add(task, 0, r);
+
+                var answer1 = new RadioButton();
+                answer1.Dock = DockStyle.Top;
+                if (!simple)
+                {
+                    answer1.Text = "этот";
+                }
+                else
+                {
+                    answer1.Text = "этот";
+                }
+
+                var answer2 = new RadioButton();
+                answer2.Dock = DockStyle.Top;
+                if (!simple)
+                {
+                    answer2.Text = "или этот";
+                }
+                else
+                {
+                    answer2.Text = "не этот";
+                }
+
+
+                radio.Controls.Add(answer1, 0, 0);
+                radio.Controls.Add(answer2, 1, 0);
+                column.Controls.Add(radio, 1, r);
+
+            }
+            var mainAnswer = new Button();
+            mainAnswer.Text = "тыкать";
+            mainAnswer.Dock = DockStyle.Top;
+            mainAnswer.Click += new EventHandler(onSubmitClick);
+            column.Controls.Add(mainAnswer, 0, column.RowCount);
+            this.Controls.Add(groupBox);
+        }
+        private void onSubmitClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("тыкнута тыкалка");
+        }
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
